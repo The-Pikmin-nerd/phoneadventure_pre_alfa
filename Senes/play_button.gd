56bf5a02e647a702f8a0e936d3cde0dd256c05a8
anonimouche-Dev <144521481+anonimouche-Dev@users.noreply.github.com>
2024-12-24 16:46:02 +0100

    updateBacround

--- Senes/play_button.gd
+++ Senes/play_button.gd
@@ -5,18 +5,20 @@ func _ready() -> void:
 	# Connect the "pressed" signal to the _on_pressed function
 	self.pressed.connect(_on_pressed)
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-
 func _on_pressed() -> void:
-	# Preload and instantiate the scene when the button is pressed
+	# Supprime la scène actuelle
+	get_tree().current_scene.queue_free()
+
+	# Précharge et instancie la nouvelle scène
 	var simultaneous_scene = preload("res://Levels/Level-1.tscn").instantiate()
+	
+	# Ajoute la nouvelle scène à la racine de l'arbre de scène
 	_add_a_scene_manually(simultaneous_scene)
 
-
 func _add_a_scene_manually(scene: Node) -> void:
-	# This is like autoloading the scene, only it happens after loading the main scene
+	# Ajoute la nouvelle scène à l'arbre de scène
 	get_tree().root.add_child(scene)
